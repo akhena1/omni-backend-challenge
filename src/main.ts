14c351c26198617c1673c4784edfde34833b619b
Gustavo Marcondes <gu.marcondes2012@gmail.com>
2024-11-03 21:27:23 -0300

    chore: create docker app container

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(morgan('common'));
   await app.listen(3000);
 }
 bootstrap();
